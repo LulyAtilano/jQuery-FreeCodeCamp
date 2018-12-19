db09874a15a4b95c37d33f65dbfd8e3ecd08d530
latilanoh@gmail.com <latilanoh@gmail.com>
2018-12-19 00:47:33 -0600

    add methods: html, remove and appendTo

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
     $("button").addClass("animated");
     $(".btn").addClass("shake");
     $("#target1").addClass("btn-primary");
-    $("button").removeClass("btn-default"); 
+    $("button").removeClass("btn-primary"); 
     $("#target1").css("color","red"); //change the css of an element using jQuery
     $("#target1").prop("disabled", true); // disable to target1
+    $("#target4").html("<em>#target4</em>"); //Change Text Inside an Element
+    $("#target4").remove(); // delete an element
+    $("#target2").appendTo("#right-well"); // move an element from a place to another
 })
