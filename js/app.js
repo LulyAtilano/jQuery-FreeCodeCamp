502f6bb92180304054b5f6d78d291f7aed43923f
latilanoh@gmail.com <latilanoh@gmail.com>
2018-12-19 00:35:41 -0600

    add methods: removeClass, css and prop

--- js/app.js
+++ js/app.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("button").addClass("animated");
     $(".btn").addClass("shake");
     $("#target1").addClass("btn-primary");
+    $("button").removeClass("btn-default"); 
+    $("#target1").css("color","red"); //change the css of an element using jQuery
+    $("#target1").prop("disabled", true); // disable to target1
 })
