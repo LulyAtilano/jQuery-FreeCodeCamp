8bec1c073179b0006af99f48673b9568a2fd96e8
latilanoh@gmail.com <latilanoh@gmail.com>
2018-12-19 00:54:15 -0600

    add methods: clone, parent and children

--- js/app.js
+++ js/app.js
@@ -8,4 +8,7 @@ $(document).ready(function(){
     $("#target4").html("<em>#target4</em>"); //Change Text Inside an Element
     $("#target4").remove(); // delete an element
     $("#target2").appendTo("#right-well"); // move an element from a place to another
+    $("#target5").clone().appendTo("#left-well"); // clone and move an element from a place to another
+    $("#target1").parent().css("background-color","red"); // add background-color to parent container of #target1
+    $("#right-well").children().css("color","orange"); // add color to children of the #right-well container
 })
